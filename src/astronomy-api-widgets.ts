4fcc23613ea479976786b8ce7a7a774c5865f02b
anuradha <astroanu2004@gmail.com>
2021-11-06 22:21:28 +0530

    Update widget

--- src/astronomy-api-widgets.ts
+++ src/astronomy-api-widgets.ts
@@ -2,34 +2,129 @@ type ConfigOptions = {
   basicToken: string;
 };
 
+type MoonPhaseParameters = {
+  element: string;
+  format: string;
+  style: {
+    moonStyle: string;
+    backgroundStyle: string;
+    backgroundColor: string;
+    headingColor: string;
+    textColor: string;
+  };
+  observer: {
+    latitude: number;
+    longitude: number;
+    date: string;
+  };
+  view: {
+    type: string;
+  };
+};
+
+type StarChartParameters = {
+  element: string;
+  style: string;
+  observer: {
+    latitude: number;
+    longitude: number;
+    date: string;
+  };
+  view: {
+    type: string;
+    parameters: {
+      position: {
+        equatorial: {
+          rightAscension: number;
+          declination: number;
+        };
+      };
+      zoom: number;
+    };
+  };
+};
+
+type APIResponse = {
+  data: {
+    imageUrl: string;
+  };
+};
+
 const AstronomyAPI = class {
   private params: ConfigOptions;
   private apiBaseUrl: string = "https://api.astronomyapi.com";
 
-  moonPhase = (
-    params: {
-      element: string;
-      format: string;
-      style: {
-        moonStyle: string;
-        backgroundStyle: string;
-        backgroundColor: string;
-        headingColor: string;
-        textColor: string;
-      };
-      observer: {
-        latitude: number;
-        longitude: number;
-        date: string;
-      };
-      view: {
-        type: string;
-      };
-    },
-    callback: Function
-  ) => {
+  private getPlaceholder(text: string) {
+    const placeholder = document.createElement("p");
+
+    placeholder.style.fontSize = "10px";
+    placeholder.style.color = "#444";
+    placeholder.style.textAlign = "center";
+    placeholder.style.verticalAlign = "middle";
+
+    placeholder.innerText = text;
+
+    return placeholder;
+  }
+
+  private setContainerSize(
+    widgetType: string,
+    el: HTMLElement,
+    viewType: string,
+  ) {
+    if (widgetType == "moon-phase") {
+      el.style.width = viewType == "portrait-simple" ? "200px" : "260px";
+      el.style.height = viewType == "portrait-simple" ? "260px" : "160px";
+    }
+
+    if (widgetType == "star-chart") {
+      el.style.width = "800px";
+      el.style.height = "617px";
+    }
+  }
+
+  private request(
+    endpoint: string,
+    params: object,
+    callback: Function,
+    el: HTMLElement,
+  ) {
     const req = new XMLHttpRequest();
 
+    const that = this;
+
+    req.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          const result = <APIResponse>JSON.parse(this.responseText);
+
+          if (el) {
+            const image = new Image();
+            image.src = result.data.imageUrl;
+            el.replaceChildren(image);
+          }
+
+          if (callback) {
+            callback(result);
+          }
+        } else if (this.status == 422) {
+          console.error(JSON.parse(this.responseText));
+        } else {
+          console.error("unknown error");
+          el.replaceChildren(that.getPlaceholder("Error"));
+        }
+      }
+    };
+
+    req.open("POST", `${this.apiBaseUrl}/${endpoint}`, true);
+
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("Authorization", `Basic ${this.params.basicToken}`);
+
+    req.send(JSON.stringify(params));
+  }
+
+  moonPhase = (params: MoonPhaseParameters, callback: Function) => {
     params = Object.assign(
       {
         element: "#moon-phase",
@@ -50,67 +145,19 @@ const AstronomyAPI = class {
           type: "portrait-simple",
         },
       },
-      params
+      params,
     );
 
-    req.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        if (this.status == 200) {
-          const result = JSON.parse(this.responseText);
+    const el = <HTMLElement>document.querySelector(params.element);
 
-          const el = document.querySelector(params.element);
+    this.setContainerSize("moon-phase", el, params.view.type);
 
-          if (el) {
-            const image = new Image();
-            image.src = result.data.imageUrl;
-            el.append(image);
-          }
+    el.append(this.getPlaceholder("Loading..."));
 
-          if (callback) {
-            callback(result);
-          }
-        } else if (this.status == 422) {
-          console.error(JSON.parse(this.responseText));
-        } else {
-          console.error("unknown error");
-        }
-      }
-    };
-
-    req.open("POST", `${this.apiBaseUrl}/api/v2/studio/moon-phase`, true);
-
-    req.setRequestHeader("Content-Type", "application/json");
-    req.setRequestHeader("Authorization", `Basic ${this.params.basicToken}`);
-
-    req.send(JSON.stringify(params));
+    this.request("api/v2/studio/moon-phase", params, callback, el);
   };
 
-  starChart = (
-    params: {
-      element: string;
-      style: string;
-      observer: {
-        latitude: number;
-        longitude: number;
-        date: string;
-      };
-      view: {
-        type: string;
-        parameters: {
-          position: {
-            equatorial: {
-              rightAscension: number;
-              declination: number;
-            };
-          };
-          zoom: number;
-        };
-      };
-    },
-    callback: Function
-  ) => {
-    const req = new XMLHttpRequest();
-
+  starChart = (params: StarChartParameters, callback: Function) => {
     params = Object.assign(
       {
         element: "#star-chart",
@@ -133,39 +180,16 @@ const AstronomyAPI = class {
           },
         },
       },
-      params
+      params,
     );
 
-    req.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        if (this.status == 200) {
-          const result = JSON.parse(this.responseText);
+    const el = <HTMLElement>document.querySelector(params.element);
 
-          const el = document.querySelector(params.element);
+    this.setContainerSize("star-chart", el, params.view.type);
 
-          if (el) {
-            const image = new Image();
-            image.src = result.data.imageUrl;
-            el.append(image);
-          }
+    el.append(this.getPlaceholder("Loading..."));
 
-          if (callback) {
-            callback(result);
-          }
-        } else if (this.status == 422) {
-          console.error(JSON.parse(this.responseText));
-        } else {
-          console.error("unknown error");
-        }
-      }
-    };
-
-    req.open("POST", `${this.apiBaseUrl}/api/v2/studio/star-chart`, true);
-
-    req.setRequestHeader("Content-Type", "application/json");
-    req.setRequestHeader("Authorization", `Basic ${this.params.basicToken}`);
-
-    req.send(JSON.stringify(params));
+    this.request("api/v2/studio/star-chart", params, callback, el);
   };
 
   constructor(params: ConfigOptions) {
